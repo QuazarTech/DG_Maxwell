25f3a248df622b963d61bb9ca3757678c74ee792
AAT <aman.iitk072@gmail.com>
2017-06-29 15:56:45 +0530

    Init files added.

--- code/lagrange_basis.py
+++ code/lagrange_basis.py
@@ -5,7 +5,7 @@ import arrayfire as af
 from utils import utils
 from scipy import special as sp
 
-import global_variables as gvar
+import app.global_variables as gvar
 
 af.set_backend('cuda')
 af.info()
@@ -15,8 +15,8 @@ def LGL_points(N):
 	Takes N as an interger input and returns N LGL points.
 	"""
 	
-	if N > 16:
-		print('Skipping! This function can only return maximum 16 LGL points.')
+	if N > 16 or N < 2:
+		print('Skipping! This function can only return from 2 to 16 LGL points.')
 		pass
 	
 	n = N - 2
@@ -31,18 +31,26 @@ def lagrange_basis(X, i, x):
     x = coordinate at which the i_{th}
         Lagrange polynomials are to be evaluated.
     '''
+    lx = 1.
     
     for m in range(X.shape[0]):
         if m != i:
-            lx *= (x-X[m])/(X[i]-X[m])
+            lx *= (x - X[m]) / (X[i] - X[m])
     
     return lx
 
 if __name__ == '__main__':
-	number_of_elements = 2
-
-	X = utils.linspace(-1, 1, number_of_elements + 1)
-
-	N = 16
-	xi_LGL = LGL_points(N)
+	number_of_elements = 15
+	
+	x = utils.linspace(-1, 1, 100)
+	af.display(X)
+	
+	xi_LGL = LGL_points(number_of_elements + 1)
 	af.display(xi_LGL)
+	
+	i = 9
+	
+	print(lagrange_basis(xi_LGL, i, x[50]))
+	
+	pass
+
