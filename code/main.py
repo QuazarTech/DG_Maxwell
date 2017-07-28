73a98ef54a54278d6c029b99fb1a3c73c36ab974
Balavarun5 <f2013462@pilani.bits-pilani.ac.in>
2017-07-28 15:48:15 +0530

    Resolving conflicts.

--- code/main.py
+++ code/main.py
@@ -12,5 +12,3 @@ if __name__ == '__main__':
 	gvar.populateGlobalVariables(8)
 	print(wave_equation.element_flux_integral(0))
 	print(af.timer.timeit(wave_equation.element_flux_integral, 0))
-	print(wave_equation.A_matrix())
-	print(gvar.x_nodes)
