756c8f6eeb4916c6d0f408c0b8793c7e6f684d24
Balavarun5 <f2013462@pilani.bits-pilani.ac.in>
2017-08-11 16:29:13 +0530

    Unit tests working.

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ af.set_backend('opencl')
 af.set_device(1)
 import numpy as np
 from app import global_variables as gvar
-import test_waveEqn
+from unit_test import test_waveEqn
 from app import wave_equation
 from app import lagrange
 
