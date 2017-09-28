8404d87fd09b815adc8bc5f179656e5cb6d4b00e
AAT <aman.iitk072@gmail.com>
2017-09-28 20:20:15 +0530

    Unit test for read_order_2_msh function added and tested. - Unit test documented.

--- 2d_solver/msh_parser.py
+++ 2d_solver/msh_parser.py
@@ -11,33 +11,33 @@ import msh_parser
 import isoparam
 import utils
 
-plt.rcParams['figure.figsize']  = 12, 7.5
-plt.rcParams['lines.linewidth'] = 1.5
-plt.rcParams['font.family']     = 'serif'
-plt.rcParams['font.weight']     = 'bold'
-plt.rcParams['font.size']       = 20  
-plt.rcParams['font.sans-serif'] = 'serif'
-plt.rcParams['text.usetex']     = True
-plt.rcParams['axes.linewidth']  = 1.5
-plt.rcParams['axes.titlesize']  = 'medium'
-plt.rcParams['axes.labelsize']  = 'medium'
+plt.rcParams['figure.figsize']     = 12, 7.5
+plt.rcParams['lines.linewidth']    = 1.5
+plt.rcParams['font.family']        = 'serif'
+plt.rcParams['font.weight']        = 'bold'
+plt.rcParams['font.size']          = 20  
+plt.rcParams['font.sans-serif']    = 'serif'
+plt.rcParams['text.usetex']        = True
+plt.rcParams['axes.linewidth']     = 1.5
+plt.rcParams['axes.titlesize']     = 'medium'
+plt.rcParams['axes.labelsize']     = 'medium'
 
-plt.rcParams['xtick.major.size'] = 8
-plt.rcParams['xtick.minor.size'] = 4
-plt.rcParams['xtick.major.pad']  = 8
-plt.rcParams['xtick.minor.pad']  = 8
-plt.rcParams['xtick.color']      = 'k'
-plt.rcParams['xtick.labelsize']  = 'medium'
-plt.rcParams['xtick.direction']  = 'in'    
+plt.rcParams['xtick.major.size']   = 8
+plt.rcParams['xtick.minor.size']   = 4
+plt.rcParams['xtick.major.pad']    = 8
+plt.rcParams['xtick.minor.pad']    = 8
+plt.rcParams['xtick.color']        = 'k'
+plt.rcParams['xtick.labelsize']    = 'medium'
+plt.rcParams['xtick.direction']    = 'in'    
 
-plt.rcParams['ytick.major.size'] = 8
-plt.rcParams['ytick.minor.size'] = 4
-plt.rcParams['ytick.major.pad']  = 8
-plt.rcParams['ytick.minor.pad']  = 8
-plt.rcParams['ytick.color']      = 'k'
-plt.rcParams['ytick.labelsize']  = 'medium'
-plt.rcParams['ytick.direction']  = 'in'
-plt.rcParams['text.usetex'] = True
+plt.rcParams['ytick.major.size']   = 8
+plt.rcParams['ytick.minor.size']   = 4
+plt.rcParams['ytick.major.pad']    = 8
+plt.rcParams['ytick.minor.pad']    = 8
+plt.rcParams['ytick.color']        = 'k'
+plt.rcParams['ytick.labelsize']    = 'medium'
+plt.rcParams['ytick.direction']    = 'in'
+plt.rcParams['text.usetex']        = True
 plt.rcParams['text.latex.unicode'] = True
 
 def read_order_2_msh(msh_file):
@@ -55,9 +55,9 @@ def read_order_2_msh(msh_file):
     
     tuple(np.ndarray, np.ndarray)
              Tuple of Nodes and Elements respectively.
-             Nodes is a :math:`N \\times 2` array, where, :math:`N` is the total
-             number of Nodes in the mesh. Each node contains it's :math:`(x, y)`
-             coordinates.
+             Nodes is a :math:`N \\times 2` array, where, :math:`N` is the
+             total number of Nodes in the mesh. Each node contains it's
+             :math:`(x, y)` coordinates.
              Elements is a :math:`N_e \\times 8` array which contains the tags
              of all the nodes which defines each element. A tag of a node is
              the array index of a node.
