90e4f16e982042da10b30b71ebd4299a0c1a088a
AAT <aman.iitk072@gmail.com>
2017-10-22 10:21:13 +0530

    A matrix function written. - Documented. - Algo not properly documented - Unit test written - Example written Issues - Unit test for test_wave_equation_2d.py::test_A_matrix and   test_waveEqn.py::test_volume_integral_flux FAILING when using opencl as backend.   Works fine on cpu and cuda backend.

--- dg_maxwell/wave_equation_2d.py
+++ dg_maxwell/wave_equation_2d.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import arrayfire as af
+
+from dg_maxwell import params
 from dg_maxwell import isoparam
 from dg_maxwell import lagrange
+from dg_maxwell import utils
+
+af.set_backend(params.backend)
 
 def dx_dxi(x_nodes, xi, eta):
     '''
@@ -208,10 +214,77 @@ def jacobian(x_nodes, y_nodes, xi, eta):
 
 def A_matrix():
     '''
+    Calculates the tensor product for the given ``params.N_LGL``.
+    A tensor product element is given by:
+    
+    .. math:: [A^{pq}_{ij}] = \\iint L_p(\\xi) L_q(\\eta) \\
+                                     L_i(\\xi) L_j(\\eta) d\\xi d\\eta
+    
+    This function finds :math:`L_p(\\xi) L_i(\\xi)` and
+    :math:`L_q(\\eta) L_j(\\eta)` and passes it to the ``integrate_2d``
+    function.
+    
+    Returns
+    -------
+    A : af.Array [N_LGL^2 N_LGL^2 1 1]
+        The tensor product.
     '''
-    N_LGL_quad   = 9
-    xi_LGL_quad  = lagrange.gauss_nodes(N_LGL_quad)
-    eta_LGL_quad = lagrange.gauss_nodes(N_LGL_quad)
     
+    N_LGL   = params.N_LGL
+    xi_LGL  = lagrange.LGL_points(N_LGL)
+    eta_LGL = lagrange.LGL_points(N_LGL)
+
+    _, Lp = lagrange.lagrange_polynomials(xi_LGL)
+    Lp = af.np_to_af_array(Lp)
+    Li = Lp.copy()
+
+    _, Lq = lagrange.lagrange_polynomials(eta_LGL)
+    Lq = af.np_to_af_array(Lq)
+    Lj = Lq.copy()
     
-    return
+    Li = af.reorder(Li, d0 = 0, d1 = 2, d2 = 1)
+    Li = af.transpose(af.tile(Li, d0 = 1, d1 = N_LGL))
+    Li = af.moddims(Li, d0 = N_LGL * N_LGL, d1 = 1, d2 = N_LGL)
+    Li = af.transpose(af.tile(Li, d0 = 1, d1 = N_LGL * N_LGL))
+    Li = af.transpose(af.moddims(af.transpose(Li),
+                                 d0 = N_LGL * N_LGL * N_LGL * N_LGL,
+                                 d1 = 1, d2 = N_LGL))
+    Li = af.reorder(Li, d0 = 2, d1 = 1, d2 = 0)
+
+    Lp = af.reorder(Lp, d0 = 0, d1 = 2, d2 = 1)
+    Lp = af.transpose(af.tile(Lp, d0 = 1, d1 = N_LGL))
+    Lp = af.moddims(Lp, d0 = N_LGL * N_LGL, d1 = 1, d2 = N_LGL)
+    Lp = af.tile(Lp, d0 = 1, d1 = N_LGL * N_LGL)
+    Lp = af.moddims(af.transpose(Lp),
+                    d0 = N_LGL * N_LGL * N_LGL * N_LGL,
+                    d1 = 1, d2 = N_LGL)
+    Lp = af.reorder(af.transpose(Lp), d0 = 2, d1 = 1, d2 = 0)
+
+    Lp_Li = af.transpose(af.convolve1(Li, Lp, conv_mode = af.CONV_MODE.EXPAND))
+    
+    Lj = af.reorder(Lj, d0 = 0, d1 = 2, d2 = 1)
+    Lj = af.tile(Lj, d0 = 1, d1 = N_LGL)
+    Lj = af.moddims(Lj, d0 = N_LGL * N_LGL, d1 = 1, d2 = N_LGL)
+    Lj = af.transpose(af.tile(Lj, d0 = 1, d1 = N_LGL * N_LGL))
+    Lj = af.moddims(af.transpose(Lj),
+                    d0 = N_LGL * N_LGL * N_LGL * N_LGL,
+                    d1 = 1, d2 = N_LGL)
+    Lj = af.reorder(Lj, d0 = 2, d1 = 0, d2 = 1)
+
+    Lq = af.reorder(Lq, d0 = 0, d1 = 2, d2 = 1)
+    Lq = af.tile(Lq, d0 = 1, d1 = N_LGL)
+    Lq = af.moddims(Lq, d0 = N_LGL * N_LGL, d1 = 1, d2 = N_LGL)
+    Lq = af.tile(Lq, d0 = 1, d1 = N_LGL * N_LGL)
+    Lq = af.moddims(af.transpose(Lq),
+                    d0 = N_LGL * N_LGL * N_LGL * N_LGL,
+                    d1 = 1, d2 = N_LGL)
+    Lq = af.reorder(af.transpose(Lq), d0 = 2, d1 = 1, d2 = 0)
+
+    Lq_Lj = af.transpose(af.convolve1(Lj, Lq, conv_mode = af.CONV_MODE.EXPAND))
+
+    A = af.moddims(utils.integrate_2d(Lp_Li, Lq_Lj,
+                                      order = 9,
+                                      scheme = 'gauss'),
+                   d0 = N_LGL * N_LGL, d1 = N_LGL * N_LGL)
+    
+    return A
