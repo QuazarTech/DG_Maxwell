4f3abe91a488c6a7a711ee549009143b790bce4e
Balavarun5 <f2013462@pilani.bits-pilani.ac.in>
2017-11-03 19:41:31 +0530

    Volume integral term debugging.

--- dg_maxwell/utils.py
+++ dg_maxwell/utils.py
@@ -478,9 +478,5 @@ def integrate_2d(poly_xi, poly_eta, order, scheme = 'gauss'):
 def polynomial_derivative(polynomial):
     '''
     '''
-    derivtive_multiplier = af.transpose(af.tile(
-        af.flip(af.range(polynomial.shape[0] - 1) + 1),
-        d0 = 1, d1 = polynomial.shape[1]))
-    
-    return polynomial[:, : -1] * derivtive_multiplier
- 
+    derivtive_multiplier = af.tile(af.transpose(af.flip( af.range(polynomial.shape[1]))), d0 = polynomial.shape[0])
+    return (polynomial * derivtive_multiplier)[:, : -1] 
